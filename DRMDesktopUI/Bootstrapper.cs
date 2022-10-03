d9ec30af99c1e89ee1fb48c692bb7a02d3ebf319
Medna Dreamheart <medozcarpaccio@gmail.com>
2022-10-03 12:49:06 +0500

    Add WPF boilerplate userinterface
    Adds WPF UI with minor tweaks using
    Caliburn

---
+++ DRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using DRMDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
