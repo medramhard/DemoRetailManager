113706f69fe99581f699791e4e2efa24cec95800
Medna Dreamheart <medozcarpaccio@gmail.com>
2022-10-29 13:33:50 +0500

    Add Authorization to Swagger

--- DRMApi/Program.cs
+++ DRMApi/Program.cs
@@ -56,6 +56,31 @@ builder.Services.AddSwaggerGen(setup =>
             Title = "Demo Retail Manager System API",
             Version = "v1"
         });
+    setup.AddSecurityDefinition("Bearer",
+        new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+    setup.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type=ReferenceType.SecurityScheme,
+                        Id="Bearer"
+                    }
+                },
+                new string[]{}
+            }
+        });
 });
 
 var app = builder.Build();
@@ -81,7 +106,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseSwagger();
-app.UseSwaggerUI( x =>
+app.UseSwaggerUI(x =>
 {
     x.SwaggerEndpoint("/swagger/v1/swagger.json", "DRM API v1");
 });
